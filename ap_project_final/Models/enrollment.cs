33340023aba871448e8bc503ba18f3bbf8892a34
meytiam <mahdivatanzadeh898@gmail.com>
2025-07-13 03:35:47 +0330

    feat: enrollment model

---
+++ ap_project_final/Models/enrollment.cs
@@ -0,0 +1,14 @@
+﻿namespace ap_project_final.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }   
+        public int StudentId { get; set; }  
+
+        public double? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
