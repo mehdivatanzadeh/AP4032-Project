f4d9f4ddf503d851bdae80ccc8706de23ac1ad59
mehdivatanzadeh <93472067+mehdivatanzadeh@users.noreply.github.com>
2025-07-13 17:00:27 +0330

    Develop (#2)
    * feat : student model
    
    * feat: professor model
    
    * feat: course model
    
    * feat: classroom model
    
    * feat: enrollment model
    
    * Feature add view (#8)
    
    * feat/login_view
    
    * feat:Admin_Dashboard_view
    
    * feat:Admin_ManageCourses_view
    
    * feat:Admin_ManageUsers_view
    
    * feat:Admin_Register_view
    
    * feat:Instructor_Dashboard_view
    
    * feat:Instructor_ManageClasses_view
    
    * fix:Instructor_Dashboard_view
    
    * feat:Student_Dashboard_view
    
    * feat:Student_ManageClasses_view
    
    * feat:Student_Profile_view
    
    ---------
    
    Co-authored-by: EhsanRzi <enrforcf@gmail.com>

---
+++ ap_project_final/Models/classroom.cs
@@ -0,0 +1,10 @@
+﻿namespace ap_project_final.Models
+{
+    public class Classroom
+    {
+        public int Id { get; set; }
+        public string Building { get; set; } 
+        public string RoomNumber { get; set; } 
+        public int Capacity { get; set; } 
+    }
+}
