8b98b48dbc832fe506a0b0602ccd51db7c548321
EhsanRzi <enrforcf@gmail.com>
2025-07-22 13:06:12 +0330

    fix : Student view (add necessaroy models)

---
+++ ap_project_final/Models/CourseDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ap_project_final.Models
+{
+    public class CourseDetailsViewModel
+    {
+        public string ClassroomNumber { get; set; }
+        public string Building { get; set; }
+        public string InstructorName { get; set; }
+        public string ClassTime { get; set; }
+        public string ExamTime { get; set; }
+        public decimal? Score { get; set; }
+    }
+}
