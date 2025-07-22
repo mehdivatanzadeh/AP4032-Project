c378c3beb062ada7ae9f690c98d45dafdc182cb5
EhsanRzi <enrforcf@gmail.com>
2025-07-22 12:55:03 +0330

    fix : Admin View (add necessary models)

---
+++ ap_project_final/Models/UserManagementViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ap_project_final.Models
+{
+    public class UserManagementViewModel
+    {
+        public List<Student> Students { get; set; }
+        public List<Professor> Professors { get; set; }
+    }
+}
